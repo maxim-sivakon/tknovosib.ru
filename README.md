5f9656d0d4583b0bf2ffe7674b19c33ea3a108f6
maxim-sivakon <m.sivakon@yandex.com>
2024-08-28 16:07:59 +0700

    save step1

---
+++ README.md
@@ -0,0 +1,9 @@
+# tknovosib.ru
+
+---
+
+## Рабочее WIKI
+1. [Доступы](wiki/connections.md)
+2. [Разработка](wiki/develop.md)
+
+---
---
+++ cleaner.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -rf /home/bitrix/ext_www/tknovosib/core/upload/resize_cache/*
+rm -rf /home/bitrix/ext_www/tknovosib/core/bitrix/managed_cache/*
+rm -rf /home/bitrix/ext_www/tknovosib/core/bitrix/stack_cache/*
+rm -rf /home/bitrix/ext_www/tknovosib/core/bitrix/backup/*
+rm -rf /home/bitrix/ext_www/tknovosib/core/bitrix/cache/*
